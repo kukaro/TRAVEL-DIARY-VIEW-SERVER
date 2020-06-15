ab5a66daf5393281ab65945d8141bbd899c39dfd
kukaro <justkukaro@naver.com>
2020-06-15 15:00:05 +0900

    add front

---
+++ project/src/front/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pages:{
+        main:{
+            entry: 'src/pages/main/main.js',
+            template: 'public/index.main.html',
+            filename: 'index.main.html'
+        },
+        test:{
+            entry: 'src/pages/test/main.js',
+            template: 'public/index.test.html',
+            filename: 'index.test.html'
+        }
+    },
+};
